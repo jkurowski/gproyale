f9890e5c5939f7d06eee736b63519938f289c094
Jacek <kontakt@jk-design.pl>
2023-06-13 01:18:49 +0200

    Poprawki cz. 3

--- resources/views/front/corporate/event/index.blade.php
+++ resources/views/front/corporate/event/index.blade.php
@@ -21,37 +21,73 @@
                     <h1>CORPORATE CONFERENCE <br>& EVENTS</h1>
                 </div>
             </div>
-
-            <div class="col-12">
-                <p class="h2">GP Royale is a leading live event and conference production company.</p>
-                <p>&nbsp;</p>
-                <p>With a wealth of experience organising conferences and events, it is our attention to detail that ensures everything runs to plan. You have the option to consult our creative media specialists regarding marketing, branding and communication, with continued support prior, during and post event.</p>
-                <p>&nbsp;</p>
-                <p>We collaborate closely with our clients to achieve their vision, co-curating programming and working with our in-house creative teams. We have a skilled and dedicated event production team that ensures the event is extraordinary in every way. From technical and digital production including AV sound and film production, and lighting design, to set build, and on-line streaming / digital platforms.</p>
-                <p>&nbsp;</p>
-                <p>Our list of international motivational speakers, music artistes and entertainment is second to none.</p>
-            </div>
-
-            <div class="col-12">
-                <div class="section-title">
-                    <h2>PAST EVENTS</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row d-flex justify-content-center">
-            <div class="col-12 col-lg-8 col-xl-6 p-0">
-                <a class="swipebox" data-type="video" rel="vimeo" href="http://vimeo.com/819495436">
-                    <img src="{{ asset('/images/video-past-events.jpg') }}" alt="VIDEO WITH PAST EVENTS" width="1200" height="849" class="m-auto">
-                </a>
-            </div>
         </div>
     </div>
 
-    <section>
+    <section class="bg-grey rwd-margin">
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <p>Corporate events are crucial for any business looking to impress their clients or to network with others in their industry. Working with us is a great opportunity to bring people together and create a platform that delivers learning, motivation and teamwork, coupled with business efficiency and improved morale, that ultimately improves our client’s bottom line.</p>
+                    <blockquote class="freight text-center mb-0 blockquote-nofooter mt-4"><b class="gold">GP ROYALE</b> <i>a leading</i> <b class="gold">LIVE EVENT</b> <i>&</i> <b class="gold">CONFERENCE</b> <br> <i>PRODUCTION COMPANY...</i></blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center mt-3 mt-sm-5">
+                    <ul class="mb-0 list-unstyled crown-list">
+                        <li>A Wealth <i>of</i> <b>Experience</b>.</li>
+                        <li><b>Attention</b> <i>to</i> Details.</li>
+                        <li><b>Collaborative</b> <i>and</i> <b>Consultative</b>.</li>
+                        <li><b>Bespoke</b> marketing <i>and</i> branding.</li>
+                        <li>Motivational <b>Speaker, Music, Entertainment</b>.</li>
+                        <li><b>Technical</b> AV production, set building and lighting.</li>
+                        <li><b>Creative</b> media specialists.</li>
+                        <li>Pre-and Post-event <b>Support</b>.</li>
+                    </ul>
+                </div>
+                <div class="col-12">
+                    <div class="separator separator-grey">
+                        <img src="{{ asset("/images/separator-grey.png") }}" alt="Section separator" width="1200" height="55">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <blockquote class="freight text-center mb-0 blockquote-nofooter mt-0"><i>EX</i>TRAORDINARY <i>in EVERY WAY</i></blockquote>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-12 col-lg-10 col-xl-8 p-0">
+                    <a class="swipebox" data-type="video" rel="vimeo" href="http://vimeo.com/819495436">
+                        <img src="{{ asset('/images/video-past-events.jpg') }}" alt="VIDEO WITH PAST EVENTS" width="1200" height="849" class="m-auto">
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 p-0">
+                    <blockquote class="freight text-center mb-0 blockquote-nofooter"><i>C</i>ORPORATE <i>E</i>VENTS <i>are</i> <i>C</i>RUCIAL. <br><i>G</i>P ROYALE <i>makes them</i> <i>E</i>XTRAORDINARY</blockquote>
+                </div>
+                <div class="col-12">
+                    <div class="separator separator-grey">
+                        <img src="{{ asset("/images/separator-grey.png") }}" alt="Section separator" width="1200" height="55">
+                    </div>
+                </div>
+                <div class="col-12 d-flex justify-content-center">
+                    <ul class="mb-0 list-unstyled crown-list">
+                        <li>Bring people <b>together</b>.</li>
+                        <li><b>Create</b> a unique learning platform.</li>
+                        <li>Motivate<b> your team</b>.</li>
+                        <li><b>Improve</b> efficiency.</li>
+                        <li><b>Strengthen</b> morale.</li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -94,17 +130,24 @@
 
     <div class="container">
         <div class="col-12">
-            <blockquote class="freight text-center blockquote-nofooter mb-0">26thNovember 2023 <br>ABU DHABI <i>GRAND PRIX</i></blockquote>
+            <blockquote class="freight text-center blockquote-nofooter mb-0">26<sup>th</sup> November 2023 <br>ABU DHABI <i>GRAND PRIX</i></blockquote>
+        </div>
+        <div class="col-12">
+            <div class="separator separator-grey">
+                <img src="{{ asset("/images/separator-grey.png") }}" alt="Section separator" width="1200" height="55">
+            </div>
         </div>
     </div>
 
-    <section>
+    <section class="pt-0">
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <p>GP Royale are able to provide both packaged and bespoke corporate conferences and events in Dubai, culminating in an F1 grand finale with access to a superyacht experience at the final race of the 2023 season at the Yas Marina circuit in Abu Dhabi on Sunday 26th November.</p>
-                    <p>&nbsp;</p>
-                    <p>Your delegates will enjoy a full day on board a luxury superyacht backing on to the racetrack, for an unforgettable party experience.</p>
+                    <div class="text-inside">
+                        <p class="center-justified">Crown your event by culminating in an F1 Grand Finale with acces to a GP Royale <br>superyacht experience at the final race 2023 season at Abu Dhabi’s Yas Marina circuit.</p>
+                        <p>&nbsp;</p>
+                        <p class="center-justified">Delegates enjoy a full day on board a luxury superyacht backing on the racetrack <br>for an unforgettable party experience.</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -112,7 +155,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 pe-0 pe-sm-3 ps-0 ps-sm-3">
                 <img src="{{ asset('/images/past-events.jpg') }}" width="1200" height="810" alt="ALT Here" class="m-auto">
             </div>
         </div>
